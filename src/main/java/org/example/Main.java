f899f5b3d7ee3e6ce0b40f61f5df43361cb335c6
doomdex84 <doomdex84@gmail.com>
2025-03-21 08:47:58 +0900

    프로젝트 세팅

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,20 @@
+package org.example;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        char i = '*';
+
+        for (int = i; i=<0; i++)
+
+
+
+
+            
+                System.out.println("Hello There");
+
+    }
+}
