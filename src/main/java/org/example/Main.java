5bfdcf90d73383d2e4a08dbff9957c93195a2921
doomdex84 <doomdex84@gmail.com>
2025-03-21 09:09:19 +0900

    피보나치수열

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,16 +5,18 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
 
-       int a = 5;
+       int a = 10;
 
-       for (int i = 0; i < a; i++) {
-           for (int j = 0; j < i; j++) {
-               System.out.print(" ");
-           }
-           for (int j = 1; j <= (a-i); j++) {
-               System.out.print(j + " ");
-           }
-           System.out.println();
+       int num1 = 0;
+       int num2 = 1;
+       int sum =  1;
+
+       for (int j=0; j < a; j++) {
+           System.out.println(sum+" ");
+           sum = num1 + num2;
+           num1 = num2;
+           num2 = sum;
+
        }
 
 
