61822440b8294d747fff7a02b78269b422ad56f6
doomdex84 <doomdex84@gmail.com>
2025-03-21 09:05:29 +0900

    역피라미드

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,15 +5,17 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
 
-        int a = 3;
-        int b = 4;
+       int a = 5;
 
-        for (int i =1; i <= a; i++){
-             for (int j=1; j <= b; j++){
-                System.out.print("*");
-            }
-            System.out.println();
-        }
+       for (int i = 0; i < a; i++) {
+           for (int j = 0; j < i; j++) {
+               System.out.print(" ");
+           }
+           for (int j = 1; j <= (a-i); j++) {
+               System.out.print(j + " ");
+           }
+           System.out.println();
+       }
 
 
     }
